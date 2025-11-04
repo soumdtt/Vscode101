8f7a91a842c2087b33da950858f58dc7a42f7b82
Soumyadeep Dutta <soumyadeep.wizkid@gmail.com>
2025-11-04 23:21:29 +0530

    fix README

--- hello.py
+++ hello.py
@@ -2,3 +2,10 @@ def say_hello(name):
     print("Hello, " + name + "!")
 
 say_hello("VSCode User")
+
+def say_day_of_week(date):
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    day_index = date.weekday()
+    print("Today is " + days[day_index] + ".")
+
+say_day_of_week(date)
